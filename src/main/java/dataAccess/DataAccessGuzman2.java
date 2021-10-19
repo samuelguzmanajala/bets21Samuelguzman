7d19d39003b0ce16633bccc34cf259bc742900e5
samuelguzmanajala <samuelguzmanajala@gmail.com>
2021-10-19 09:41:55 +0200

    coverage

---
+++ src/main/java/dataAccess/DataAccessGuzman2.java
@@ -0,0 +1,108 @@
+package dataAccess;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import configuration.ConfigXML;
+import domain.Event;
+import domain.Question;
+
+public class DataAccessGuzman2 {
+	protected static EntityManager db;
+	protected static EntityManagerFactory emf;
+	static String adm = "admin";
+
+	ConfigXML c = ConfigXML.getInstance();
+
+	public DataAccessGuzman2(boolean initializeMode) {
+
+		System.out.println("Creating DataAccess instance => isDatabaseLocal: " + c.isDatabaseLocal()
+		+ " getDatabBaseOpenMode: " + c.getDataBaseOpenMode());
+
+		open(initializeMode);
+
+	}
+
+	public DataAccessGuzman2() {
+		new DataAccess(false);
+	}
+	public void open(boolean initializeMode) {
+
+		System.out.println("Opening DataAccess instance => isDatabaseLocal: " + c.isDatabaseLocal()
+		+ " getDatabBaseOpenMode: " + c.getDataBaseOpenMode());
+
+		String fileName = c.getDbFilename();
+		if (initializeMode) {
+			fileName = fileName + ";drop";
+			System.out.println("Deleting the DataBase");
+		}
+
+		if (c.isDatabaseLocal()) {
+			emf = Persistence.createEntityManagerFactory("objectdb:" + fileName);
+			db = emf.createEntityManager();
+		} else {
+			Map<String, String> properties = new HashMap<String, String>();
+			properties.put("javax.persistence.jdbc.user", c.getUser());
+			properties.put("javax.persistence.jdbc.password", c.getPassword());
+
+			emf = Persistence.createEntityManagerFactory(
+					"objectdb://" + c.getDatabaseNode() + ":" + c.getDatabasePort() + "/" + fileName, properties);
+
+			db = emf.createEntityManager();
+		}
+
+	}
+	public void close() {
+		db.close();
+		System.out.println("DataBase closed");
+	}
+	
+	public boolean deleteEvent(Event event) {
+		if (event == null)
+			throw new NullPointerException("The event is null");
+
+		if (db.find(Event.class, event.getEventNumber()) == null) {
+			System.out.println("El evento no se encuentra en la base de datos");
+			return false;
+		}
+
+		String deleteEventSql = "DELETE FROM Event e WHERE e.getEventNumber()=?1";
+		String selectAllQuestionSql = "SELECT qu FROM Question qu";
+
+		Query query = db.createQuery(deleteEventSql, Question.class);
+
+		query.setParameter(1, event.getEventNumber());
+
+		TypedQuery<Question> query2 = db.createQuery(selectAllQuestionSql, Question.class);
+		try {
+			List<Question> allDbQuestions = query2.getResultList();
+			db.getTransaction().begin();
+			for (Question question : allDbQuestions) {
+				if (question.getEvent().equals(event)) {
+					db.remove(question);
+					System.out.println("La pregunta:"+question.getQuestion()+" ha sido ELIMINADA");
+				} else {
+					System.out.println("La pregunta "+question.getQuestion()+" No ha sido ELIMINADA");
+				}
+			}
+			query.executeUpdate();
+			db.getTransaction().commit();
+			System.out.println("Eveno Eliminado: " + event); 
+
+			return true;
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+
+	}
+
+
+}
---
+++ src/test/java/DeleteEventDA2Test.java
@@ -0,0 +1,137 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import configuration.ConfigXML;
+import configuration.UtilDate;
+import dataAccess.DataAccessGuzman2;
+import domain.Event;
+import utility.TestUtilityDataAccess;
+
+class DeleteEventDA2Test {
+
+	static DataAccessGuzman2 sut = new DataAccessGuzman2(ConfigXML.getInstance().getDataBaseOpenMode().equals("initialize"));
+	static TestUtilityDataAccess testDA = new TestUtilityDataAccess();
+
+	/*
+	 * No se como comprobar si los outputs son los correctos con un assert pero son los correctos. 
+	 */
+	@Test
+	@DisplayName("Caso 1: Eliminar evento cuando hay otros eventos con preguntas en la BD")
+	@Order(1)
+	void testDeleteEventA() {
+		java.util.Date d = UtilDate.newDate(2019, 10, 17);
+		Event ev1= new Event(1,"Atletico-Atletic", d);
+		Event ev2= new Event(2, "Madrid-Atletic", d);
+		Event ev3= new Event(3, "Atletico-Barcelona", d);
+		Event ev4= new Event(4,"Atletico-Atletic", d);
+		testDA.open();
+		testDA.addEvent(ev1);
+		testDA.addEvent(ev2);
+		testDA.addEvent(ev3);
+		testDA.addEvent(ev4);
+
+		try {
+			testDA.createQuestion(ev3, "Quien ganara", 0.3f);
+			testDA.createQuestion(ev4, "Empate", 0.5f);
+			testDA.close();
+			boolean expected = true;
+			System.out.println("Output: ");
+			boolean actual = sut.deleteEvent(ev1);
+			System.out.println("/FIN");
+			assertEquals(expected, actual);
+			System.out.println("Devolviendo datos a la base de datos");
+			testDA.open();
+			testDA.removeEvent(ev2);
+			testDA.removeEvent(ev3);
+			testDA.removeEvent(ev4);
+			testDA.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			testDA.close();
+			fail("no se ha podido realizar el test correctamente");
+		}
+	}
+	@Test
+	@DisplayName("Caso 2: Eliminar Evento con 1 pregunta")
+	@Order(2)
+	void testDeleteEventB() {
+		java.util.Date d = UtilDate.newDate(2019, 10, 17);
+		Event ev1= new Event(1,"Atletico-Atletic", d);
+		Event ev2= new Event(2, "Madrid-Atletic", d);
+		Event ev3= new Event(3, "Atletico-Barcelona", d);
+		Event ev4= new Event(4,"Atletico-Atletic", d);
+		testDA.open();
+		testDA.addEvent(ev1);
+		testDA.addEvent(ev2);
+		testDA.addEvent(ev3);
+		testDA.addEvent(ev4);
+
+		try {
+			testDA.createQuestion(ev3, "Quien ganara", 0.3f);
+			testDA.createQuestion(ev4, "Empate", 0.5f);
+			testDA.close();
+			boolean expected = true;
+			System.out.println("Output: ");
+			boolean actual = sut.deleteEvent(ev4);
+			System.out.println("/FIN");
+			assertEquals(expected, actual);
+			System.out.println("Devolviendo datos a la base de datos");
+			testDA.open();
+			testDA.removeEvent(ev1);
+			testDA.removeEvent(ev2);
+			testDA.removeEvent(ev3);
+			testDA.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			testDA.close();
+			fail("no se ha podido realizar el test correctamente");
+		}
+	}
+	
+	
+
+	@Test
+	@DisplayName("Caso 3: evento nulo")
+	@Order(3)
+	void testDeleteEvent4() throws NullPointerException {
+		Event ev1 = null;
+		NullPointerException exception = assertThrows(NullPointerException.class, () -> {
+			sut.deleteEvent(ev1);
+		});
+		String actual = exception.getMessage();
+		String expected = "The event is null";
+		assertEquals(expected, actual);
+		testDA.close();
+	}
+	
+	
+
+	@Test
+	@DisplayName("Caso 4: El evento no se encuentra en la base de datos")
+	@Order(4)
+	void testDeleteEvent3() {
+		java.util.Date d = UtilDate.newDate(2019, 10, 17);
+		Event ev1 = new Event(3, "Atletico-Madrid", d);
+		boolean expected = sut.deleteEvent(ev1);
+		boolean obtained = false;
+		assertEquals(expected, obtained);
+	}
+
+	@Test
+	@DisplayName("Caso 6: Eliminar evento sin Pregunta")
+	@Order(5)
+	void testDeleteEvent7() {
+		java.util.Date d = UtilDate.newDate(2019, 10, 17);
+		Event ev1 = new Event(3, "Atletico-Madrid", d);
+		testDA.open();
+		testDA.addEvent(ev1);
+		testDA.close();
+		boolean expected = sut.deleteEvent(ev1);
+		boolean obtained = true;
+		assertEquals(expected, obtained);
+	}
+
+}
